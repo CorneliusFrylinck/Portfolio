621c5b9b0a15f6b8e7c84891378e5ad631b75860
CorneliusFrylinck <cfrylinck1997@gmail.com>
2021-09-27 22:56:03 +0200

    Added anchor hrefs
    For error: The href attribute is required for an anchor to be keyboard accessible

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { LinkedinOutlined, GooglePlusOutlined, GithubOutlined } from '@ant-design/icons';
 
-//<li className="txt skills"><a>Skills</a></li> '../src/img/me.png' <PostCard post={post} />
+//<li className="txt skills"><a href="/#">Skills</a></li> '../src/img/me.png' <PostCard post={post} />
 
 function App() {
   return (
@@ -11,9 +11,9 @@ function App() {
         <div className="main">
           <nav>
             <ul className="nav-menu">
-              <li id="home" className="txt hm hover active"><a>Home</a></li>
-              <li className="txt proj"><a>Projects</a></li>
-              <li className="txt about"><a>About</a></li>
+              <li id="home" className="txt hm hover active"><a href="/#">Home</a></li>
+              <li className="txt proj"><a href="/#">Projects</a></li>
+              <li className="txt about"><a href="/#">About</a></li>
             </ul>
           </nav>
           <div id="homePage" className="homePage">
@@ -27,40 +27,40 @@ function App() {
             <h1 className="txt">Favorite 2020 Projects</h1>
             <h2 className="txt">Completed - at least for now ;)</h2>
             <p className="projItem hovertxt">
-              <a className="stxt" href="https://github.com/Cornelius27584046/python-Djarvis"><GithubOutlined /> Python Djarvis
+              <a href="/#" className="stxt" href="https://github.com/Cornelius27584046/python-Djarvis"><GithubOutlined /> Python Djarvis
                 <img className="hoverimg" src={require('../src/img/django.png')} alt="please use a different browser"/>
               </a><br/>
-              
-              <a>Djarvis is a small console project that is meant to be a development-orientated version of Iron Man's Jarvis program. Djarvis starts on a loop, listening until it hears the word "open". After the word has been said, the program asks what you want to do, gets the information regarding what you want to do and thereafter opens the pre-designated programs. For example, when I want to work on web development, Djarvis opens vsCode and my web browser.</a>
+
+              <a href="/#">Djarvis is a small console project that is meant to be a development-orientated version of Iron Man's Jarvis program. Djarvis starts on a loop, listening until it hears the word "open". After the word has been said, the program asks what you want to do, gets the information regarding what you want to do and thereafter opens the pre-designated programs. For example, when I want to work on web development, Djarvis opens vsCode and my web browser.</a>
             </p>
             <p className="projItem hovertxt">
-              <a className="stxt" href="https://github.com/Cornelius27584046/python-Connect-4"><GithubOutlined /> Python Connect 4
+              <a href="/#" className="stxt" href="https://github.com/Cornelius27584046/python-Connect-4"><GithubOutlined /> Python Connect 4
                 <img className="hoverimg" src={require('../src/img/connect4.png')} alt="please use a different browser"/>
               </a><br/>
-              <a>A connect 4 game that is written in Python3 where you play against an AI that is written using the MiniMax algorithm.</a>
+              <a href="/#">A connect 4 game that is written in Python3 where you play against an AI that is written using the MiniMax algorithm.</a>
             </p>
             <p className="projItem hovertxt">
-              <a className="stxt" href="https://github.com/Cornelius27584046/unity3D-Arena-Game"><GithubOutlined /> Unity 3D Arena Game
+              <a href="/#" className="stxt" href="https://github.com/Cornelius27584046/unity3D-Arena-Game"><GithubOutlined /> Unity 3D Arena Game
                  </a><br/>
-              <a>Small but cool 3D game written by following a tutorial by AwesomeTuts.</a>
+              <a href="/#">Small but cool 3D game written by following a tutorial by AwesomeTuts.</a>
             </p>
             <p className="projItem">
-              <a className="stxt hovertxt" href="http://luxitlegalbill.co.za/"> Luxit Legal Bill
+              <a href="/#" className="stxt hovertxt" href="http://luxitlegalbill.co.za/"> Luxit Legal Bill
                 <img className="hoverimg" src={require('../src/img/luxit.png')} alt="please use a different browser"/>
               </a><br/>
-              <a>
+              <a href="/#">
                 A legal bill of cost generator that has a login system and allows users to create bills of cost by entering the details of each item one at a time, viewing the product in a table view and finally downloading a pdf-version of the bill. <br/>
                 Written in HTML, CSS, JS and PHP.
               </a>
             </p>
             <h2 className="txt">In Progress</h2>
             <p className="projItem">
-              <a className="stxt"> name: to be decided</a><br/>
-              <a>Unity 3D Endless runner game where you play as a slime. In the beginning all you can do is dodge, but as you level up you will start learning how to attack.</a>
+              <a href="/#" className="stxt"> name: to be decided</a><br/>
+              <a href="/#">Unity 3D Endless runner game where you play as a slime. In the beginning all you can do is dodge, but as you level up you will start learning how to attack.</a>
             </p>
             <p className="projItem">
-              <a className="stxt"> name: to be decided</a><br/>
-              <a>
+              <a href="/#" className="stxt"> name: to be decided</a><br/>
+              <a href="/#">
                 Text-based RTS game. The Earth is dying fast so your country has sent you to claim a planet in space. The only problem is that other countries had the same idee...<br/>
                 The ship can't make it back, so your only option is to fight. First you build a base, then an army. <br/>
                 Written in HTML, CSS, JSS with an Express backend that uses sockets.
@@ -74,7 +74,7 @@ function App() {
             <p>
               This website was written using React, CSS, JS and is hosted to IBM Cloud with a CI/CD pipeline. <br/>
               The background was made by creating a canvas and controlling it with JS. <br/> PS. Try clicking and holding your left mouse button. <br />
-              
+
 
             </p>
             <h2>About the owner:</h2>
@@ -92,8 +92,8 @@ function App() {
           </div>
         </div>
         <footer style={{ width: '100vw', left: 0 }}>
-          <div><a className="hovertxt" href="https://www.linkedin.com/in/cornelius-frylinck-710698188"><LinkedinOutlined /> Cornelius Frylinck</a></div>
-          <div><a className="hovertxt" href="https://github.com/Cornelius27584046"><GithubOutlined /> Cornelius27584046</a></div>
+          <div><a href="/#" className="hovertxt" href="https://www.linkedin.com/in/cornelius-frylinck-710698188"><LinkedinOutlined /> Cornelius Frylinck</a></div>
+          <div><a href="/#" className="hovertxt" href="https://github.com/Cornelius27584046"><GithubOutlined /> Cornelius27584046</a></div>
           <div><GooglePlusOutlined /> cfrylinck1997@gmail.com</div>
           <div style={{ position: 'absolute', float: 'left', bottom: 0, padding: 5 + 'px'}}>&copy; Cornelius Frylinck - 2020</div>
         </footer>
