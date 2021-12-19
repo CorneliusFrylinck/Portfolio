b7aa877f4413f7e151e12af70c7cca5a69061bb7
CorneliusFrylinck <cfrylinck1997@gmail.com>
2021-12-19 13:43:13 +0200

    Added extra space on the bottom

--- src/App.js
+++ src/App.js
@@ -96,11 +96,12 @@ function App() {
                 </a><br/>
                 <a href="/#">
                   An existing system written by a different developer at iPlan is used by a client to manage their fruit procurement.<br/>
-                  The client decided they wanted a few changes on the current system to be able to use it for their <a className="hovertxt" href="/#">vegetable produrement.
+                  The client decided they wanted a few changes on the current system to be able to use it for their <a className="stxt hovertxt" href="/#">vegetable produrement.
                     <img className="hoverimg" src={require('../src/img/vegIntake1.png')} alt="please use a different browser"/>
                   </a>
                 </a>
               </p>
+              <br /><br /><br /><br />
               <br /><br /><br /><br />
             </div>
             <div id="aboutPage" className="aboutPage hidden">
